ad54a5d86c1bd404b0ccabd4ed9e8629dcb90994
arguxx <argianradityaputra21@gmail.com>
2024-03-06 23:01:27 +0700

    arrange new directory

---
+++ devops-example/java/src/main/java/com/tabeldata/templateproject/TemplateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tabeldata.templateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateProjectApplication.class, args);
+	}
+
+}
